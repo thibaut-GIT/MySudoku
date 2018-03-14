0cfd47ddca238792a13184233c44a891aff107a2
Unknown <thibaut.prudhomme@epsi.fr>
2018-03-14 14:08:00 +0100

    Init project

---
+++ app/src/main/java/com/example/thibaut/mysudoku/LevelActivity.java
@@ -0,0 +1,13 @@
+package com.example.thibaut.mysudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LevelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+    }
+}
