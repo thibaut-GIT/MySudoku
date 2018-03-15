4de2f9c7684fc456374c47b08f92cee11b701afd
Unknown <thibaut.prudhomme@epsi.fr>
2018-03-15 14:47:23 +0100

    Game + Grid

--- app/src/main/java/com/example/thibaut/mysudoku/GameActivity.java
+++ app/src/main/java/com/example/thibaut/mysudoku/GameActivity.java
@@ -8,6 +8,6 @@ public class GameActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(new Dessin(this));
     }
 }
--- app/src/main/java/com/example/thibaut/mysudoku/ListActivity.java
+++ app/src/main/java/com/example/thibaut/mysudoku/ListActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,15 +20,24 @@ public class ListActivity extends Activity {
         //Generation du tableau d'objet
         int tab[][] = new int[300][4];
         int idItem = 0;
-        for (int level=1; level<=3; level++) {
+        for (int level = 1; level <= 3; level++) {
             for (int i = 0; i <= 99; i++) {
                 for (int j = 0; j <= 3; j++) {
-                    switch(j) {
-                        case 0: tab[idItem][j] = idItem; break;
-                        case 1: tab[idItem][j] = level; break;
-                        case 2: tab[idItem][j] = i; break;
-                        case 3: tab[idItem][j] = (int)(Math.random()*(101)); break;
-                        default: break;
+                    switch (j) {
+                        case 0:
+                            tab[idItem][j] = idItem;
+                            break;
+                        case 1:
+                            tab[idItem][j] = level;
+                            break;
+                        case 2:
+                            tab[idItem][j] = i;
+                            break;
+                        case 3:
+                            tab[idItem][j] = (int) (Math.random() * (101));
+                            break;
+                        default:
+                            break;
                     }
                 }
                 idItem++;
@@ -42,14 +52,23 @@ public class ListActivity extends Activity {
         final ListView listview = (ListView) findViewById(R.id.listeId);
         String[] values = new String[100];
         int compteur = 0;
-        for (int i=0; i<tab.length; i++) {
-                if (tab[i][1] == level) {
-                    values[compteur] =  Integer.toString(tab[i][2]) + " - " + Integer.toString(tab[i][3]);
-                    compteur++;
-                }
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i][1] == level) {
+                values[compteur] = Integer.toString(tab[i][2]) + " - " + Integer.toString(tab[i][3]);
+                compteur++;
+            }
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,android.R.id.text1,values);
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
         listview.setAdapter(adapter);
+
+        //From LIST to GAME
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ListActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
